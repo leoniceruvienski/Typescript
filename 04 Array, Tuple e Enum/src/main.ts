eff4ba13d7a3d1ffc78696af496e2365c4459a57
Leonice <leoniceruvienski@gmail.com>
2022-02-14 12:35:05 -0200

    Aula 04

---
+++ 04 Array, Tuple e Enum/src/main.ts
@@ -0,0 +1,21 @@
+// Array, Tuple, Enum
+// Array
+
+let items: Array<number>
+items = [1, 2, 3, 4, 'estudar']
+
+
+let items: number[]
+items = [1, 2, 3, 4]
+
+// // Tuple
+let items: [number, string, boolean]
+items = [1, 'cataline', true]
+
+// // enum
+enum colors {
+    white = '#fff',
+    black= '#000'
+}
+let white: colors = colors.black
+console.log(white)
