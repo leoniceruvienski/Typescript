bd96777b5b67684a3f6201ebde3e710e9d6a9300
Leonice <leonice@pop-os.localdomain>
2022-03-07 08:57:22 -0300

    Aula 09

---
+++ 09 Interfaces/src/main.ts
@@ -0,0 +1,65 @@
+// Mostrar a cidade do usuário
+function showCity(user: any) {
+    return user.city
+}
+
+interface User {
+    name: string,
+    address: {
+        city: string
+        UF: string
+    }
+}
+
+function showCity(user: User) {
+    return user.address.city
+}
+
+
+type UF = 'PR' | 'SP' | 'MG'
+
+interface User {
+    name: string,
+    address: {
+        city: string
+        UF: UF
+    }
+}
+
+function showCity(user: User) {
+    user.address.UF = 'PR'
+    return user.address.city
+}
+
+
+type UF = 'PR' | 'SP' | 'MG'
+
+interface User {
+    name: string,
+    address: {
+        city: string
+        UF: UF
+    }
+    sayHello: () => void
+}
+
+function showCity(user: User) {
+    user.sayHello = () => console.log('Programação')
+    return user.address.city
+}
+
+
+type UF = 'PR' | 'SP' | 'MG'
+
+interface User {
+    name: string,
+    address: {
+        city: string
+        UF: UF
+    }
+    sayHello: () => void
+}
+
+let users: User[]
+users[1].address.city // dessa forma cria  a lista de usuários
+
